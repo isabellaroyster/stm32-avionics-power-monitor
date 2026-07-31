4ccd86bc340fc6f93ebe893d07db9be475d1c701
isabellaroyster <isabella.royster@gmail.com>
2026-07-31 13:10:46 -0400

    add onboard LED blink firmware
    Added GPIO toggle logic and a 500 ms delay for the NUCLEO-G071RB onboard LED. The firmware compiles successfully and is awaiting physical hardware validation.

--- firmware/avionics_power_monitor/Core/Src/main.c
+++ firmware/avionics_power_monitor/Core/Src/main.c
@@ -100,6 +100,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
+	HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
